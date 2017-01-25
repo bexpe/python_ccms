1857a0f1ef83140d6819b4ad27fae7f153e00871
Tomasz Bujakowski <bujakowski@gmail.com>
2017-01-25 11:19:12 +0100

    User set_name set_surname added

--- users.py
+++ users.py
@@ -1,10 +1,25 @@
 class User:
     def __init__(self, name, surname):
+        if len(name) == 0 or len(surname) == 0:
+            raise ValueError('Name and surname cannot be empty.')
+
         self.name = name
         self.surname = surname
 
         self.username = self.name[:2] + self.surname[:3]
         self.password = self.username.lower()
+
+    def change_password(self, new_password):
+        self.password = new_password
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_surname(self, new_surname):
+        self.surname = new_surname
+
+
+
 
 class Student(User):
     pass
@@ -21,5 +36,5 @@ class Menager(Employee):
 class Mentor(Employee):
     pass
 
-user = User('Tomek', 'Bujakowski')
+user = User('', 'Bujakowski')
 print(user.__dict__)
