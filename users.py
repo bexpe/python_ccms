94132c5b2270422e7d5c322ea2c6ad6d81afe44c
Beata <beata.k.puzio@gmail.com>
2017-01-25 13:03:50 +0100

    class method to print a list

--- users.py
+++ users.py
@@ -1,14 +1,15 @@
 class User:
+    _user_list = []
     def __init__(self, name, surname):
         if len(name) == 0 or len(surname) == 0:
             raise ValueError('Name and surname cannot be empty.')
-
+        self.id = len(self._user_list) + 1
         self.name = name
         self.surname = surname
 
         self.username = self.name[:2] + self.surname[:3]
         self.password = self.username.lower()
-
+        self._user_list.append(self)
     def change_password(self, new_password):
         self.password = new_password
 
@@ -18,8 +19,13 @@ class User:
     def set_surname(self, new_surname):
         self.surname = new_surname
 
+    def remove_from_list(self, object):
+        self._user_list.remove(object)
 
-
+    @classmethod
+    def print_object(cls):
+        for line in cls._user_list:
+            print(line.name)
 
 class Student(User):
     pass
@@ -36,5 +42,7 @@ class Menager(Employee):
 class Mentor(Employee):
     pass
 
-user = User('', 'Bujakowski')
-print(user.__dict__)
+user = User('Tomasz', 'Bujakowski')
+a = User("bla", "bla")
+b = User('blaaaa', 'blaaaaaa')
+user.print_object()
