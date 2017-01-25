5673dec16c2c9b93b4610a540f353b6d9d630f5a
Tomasz Bujakowski <bujakowski@gmail.com>
2017-01-25 16:06:52 +0100

    csv files read and write for persons classes added

--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, id=0):
         if len(name) == 0 or len(surname) == 0:
             raise ValueError('Name and surname cannot be empty.')
 
@@ -26,36 +26,138 @@ class User:
         for line in cls._user_list:
             print(line.name)
 
+    @classmethod
+    def list_to_csv(cls):
+        table = cls.objects_to_list(cls)
+        with open(cls.FILE, 'w') as file:
+            for record in table:
+                row = ','.join(record)
+                file.write(row + "\n")
+
+
 class Student(User):
     _students_list = []
+    FILE = 'data/students.csv'
+
     def __init__(self, name, surname):
-        super().__init__(name,surname)
+        super().__init__(name, surname)
+
         self.id = len(self._students_list) + 1
         self._students_list.append(self)
+        self.grades = {}
+
+    def objects_to_list(self):
+        list_to_write = []
+
+        for student in self._students_list:
+            list_to_write.append([str(student.id), student.name, student.surname])
+        return list_to_write
+
+    @classmethod
+    def save_students_csv(cls):
+        cls.list_to_csv()
+
+    @classmethod
+    def load_students_csv(cls):
+
+        import csv
+        with open(cls.FILE, 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+
+            for line in reader:
+                Student(line[1], line[2])
 
 
 class Employee(User):
-    _manager_list = []
+    _employee_list = []
+    FILE = 'data/employees.csv'
+
     def __init__(self, name, surname):
-        super().__init__(name,surname)
-        self.id = len(self._manager_list) + 1
-        self._manager_list.append(self)
+        super().__init__(name, surname)
+        self.id = len(self._employee_list) + 1
+
+        if self.__class__ == Employee:
+            self._employee_list.append(self)
+
+    def objects_to_list(self):
+        list_to_write = []
+
+        for person in self._employee_list:
+            list_to_write.append([str(person.id), person.name, person.surname])
+        return list_to_write
+
+    @classmethod
+    def save_employees_csv(cls):
+        cls.list_to_csv()
+
+    @classmethod
+    def load_employees_csv(cls):
+
+        import csv
+        with open(cls.FILE, 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            for line in reader:
+                Employee(line[1], line[2])
 
 
 class Manager(Employee):
     _manager_list = []
+    FILE = 'data/managers.csv'
+
     def __init__(self, name, surname):
-        super().__init__(name,surname)
+        super().__init__(name, surname)
         self.id = len(self._manager_list) + 1
         self._manager_list.append(self)
+
+    def objects_to_list(self):
+        list_to_write = []
+
+        for person in self._manager_list:
+            list_to_write.append([str(person.id), person.name, person.surname])
+        return list_to_write
+
+    @classmethod
+    def save_manager_csv(cls):
+        cls.list_to_csv()
+
+    @classmethod
+    def load_manager_csv(cls):
+        import csv
+        with open(cls.FILE, 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            for line in reader:
+                Manager(line[1], line[2])
 
 
 class Mentor(Employee):
     _mentor_list = []
+    FILE = 'data/mentors.csv'
+
     def __init__(self, name, surname):
-        super().__init__(name,surname)
+        super().__init__(name, surname)
         self.id = len(self._mentor_list) + 1
         self._mentor_list.append(self)
 
+    def objects_to_list(self):
+        list_to_write = []
+
+        for person in self._mentor_list:
+            list_to_write.append([str(person.id), person.name, person.surname])
+        return list_to_write
+
+    @classmethod
+    def save_mentor_csv(cls):
+        cls.list_to_csv()
+
+    @classmethod
+    def load_mentor_csv(cls):
+        import csv
+        with open(cls.FILE, 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            for line in reader:
+                Mentor(line[1], line[2])
+
+
 class Attedance:
     pass
+
