bdea8ce4eaf953074524e0e5b87f0743919939c8
Tomasz Bujakowski <bujakowski@gmail.com>
2017-01-25 10:36:06 +0100

    skeleton of classes added

---
+++ ui.py
@@ -0,0 +1,18 @@
+class Menu:
+    pass
+
+
+class StudentUI(Menu):
+    pass
+
+
+class EmployeeUI(Menu):
+    pass
+
+
+class ManagerUI(EmployeeUI):
+    pass
+
+
+class MentorUI(EmployeeUI):
+    pass
---
+++ users.py
@@ -0,0 +1,18 @@
+class User:
+    pass
+
+
+class Student(User):
+    pass
+
+
+class Employee(User):
+    pass
+
+
+class Menager(Employee):
+    pass
+
+
+class Mentor(Employee):
+    pass
