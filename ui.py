c016552a85d2003aa20a33197b77cc1e2182a8cb
Stefan Kania <stefankania95@gmail.com>
2017-01-26 11:08:49 +0100

    Student_assigment_ui method implemented

--- ui.py
+++ ui.py
@@ -1,3 +1,6 @@
+from assigments import Assigments
+
+
 class StudentUI():
 
     def __init__(self, student):
@@ -16,18 +19,18 @@ class StudentUI():
             )
             user_choose = input('Your choose: ')
             if user_choose == "1":
-                self.show_student_grade_ui():
+                self.show_student_grade_ui()
             elif user_choose == "2":
-                self.show_assigments_ui():
+                self.show_assigments_ui()
             elif user_choose == "3":
-                self.submit_assigment_ui():
+                self.submit_assigment_ui()
             elif user_choose == "0":
                 quit()
             else:
                 print("Bad choice. Enter correct value.")
 
     def show_student_grade_ui(self):
-    	student_grades = self.student.get_student_grades()
+        student_grades = self.student.get_student_grades()
         student_grades_table = ["| {} : {} %".format(key, value) for key, value in student_grades.items()]
 
         print((
@@ -40,10 +43,33 @@ class StudentUI():
         ))
 
     def show_assigments_ui(self):
-        pass
+        assigments = Assigments.get_assigments_list()
+        assigments_table = ["| {}".format(assigment.get_assigment_name()) for assigment in assigments]
+
+        print((
+            "\n/---------------------"
+            "\n| Assigments:"
+            "{}"
+            "\n\---------------------"
+        ).format(
+            ''.join(assigments_table)
+        ))
 
     def submit_assigment_ui(self):
-        pass
+        print(
+            "\n/---------------------"
+            "\n| Submit assigment"
+            "\n|"
+        )
+        assigment_name = input('| Assigment name: ')
+        assigment = Assigment.get_assigment_by_name(assigment_name)
+        if assigment:
+            student_solution = input('| Insert your solution(github link): ')
+            assigment.submit_assigment_answer(self.student.get_student_id(), student_solution)
+            print(" *** Solution added *** ")
+        else:
+            print(" *** Assigment not found *** ")
+
 
 
 class EmployeeUI:
@@ -70,11 +96,11 @@ class EmployeeUI:
     def show_student_list(self):
         # Student.table
         print("\n/---------------------"
-        "\n| Employee menu:"
-        "\n| (1) Show student details"
-        "\n| (2) Return to menu"
-        "\n| (0) Exit"
-        "\n\---------------------")
+              "\n| Employee menu:"
+              "\n| (1) Show student details"
+              "\n| (2) Return to menu"
+              "\n| (0) Exit"
+              "\n\---------------------")
         employee_table_option = input("Choose option")
         if employee_table_option == "1":
             self.employee_menu()
@@ -91,6 +117,7 @@ class ManagerUI(EmployeeUI):
 
 
 class MentorUI(EmployeeUI):
+
     def __init__(self, mentor):
         super().__init__(mentor)
 
@@ -122,7 +149,6 @@ class MentorUI(EmployeeUI):
             quit()
         else:
             print("You need to choose from options: ")
-
 
     def add_assignment(self):
         pass
