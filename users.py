37c4a305bc8c8b664eea239d821faf1317447e56
Beata <beata.k.puzio@gmail.com>
2017-01-26 14:08:29 +0100

    classmethod changed

--- users.py
+++ users.py
@@ -249,8 +249,9 @@ class Student(User):
         cls.get_student_from_list_by_id()
         student
 
-    def get_students_objects(self):
-        return self._students_list
+    @classmethod
+    def get_students_objects(cls):
+        return cls._students_list
 
 class Employee(User):
     _employee_list = []
