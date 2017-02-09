d94db612424b164e14b3b7cf7ac3e84aa4b7ab5b
sebmichalowski <seb.michalowski@gmail.com>
2017-02-09 14:10:49 +0100

    AttendanceModel: db_get_attendance_by_id(student_id), db_get_count_attendance_values(student_id), db_get_attendance_values_sum(student_id)

--- model/attendance_model.py
+++ model/attendance_model.py
@@ -2,8 +2,9 @@ import sqlite3
 
 
 class AttendanceModel:
-
-    # TEMP BY SEBASTIAN
+    """
+    MCV Model class for attendance, connection to database.
+    """
     @staticmethod
     def add_assignment_to_db(assignment_object):
         connect = sqlite3.connect('data/data.db')
@@ -17,11 +18,46 @@ class AttendanceModel:
             print(row)
 
     @staticmethod
-    def check_attendance_by_id_model(student_id):
+    def db_get_attendance_by_id(student_id):
+        """
+        Get attendance list by student id.
+        :param student_id:
+        :return: attendance_list_by_student
+        """
         connect = sqlite3.connect('data/data.db')
         cur = connect.cursor()
         cur.execute("SELECT Attendance_value, Date FROM Student_Attendance WHERE Student_id = (?)", student_id)
         connect.commit()
+        attendance_list_by_student = cur.fetchall()
+        return attendance_list_by_student
 
     @staticmethod
-    def count_attendance_values_model(student_id):
+    def db_get_count_attendance_values(student_id):
+        """
+        Get from database counted attendance values by id.
+        :param student_id:
+        :return: counted_attendance_values (object)
+        """
+        connect = sqlite3.connect('data/data.db')
+        cur = connect.cursor()
+        cur.execute("SELECT Attendance_value, COUNT(Attendance_value) from Student_Attendance "
+                    "WHERE Student_id = (?)"
+                    "GROUP BY Attendance_value ORDER BY Attendance_value ASC;", student_id)
+        connect.commit()
+        counted_attendance_values = cur.fetchall()
+        return counted_attendance_values
+
+    @staticmethod
+    def db_get_attendance_values_sum(student_id):
+        """
+        get_attendance_values_sum
+        :param student_id:
+        :return: attendance_by_student_id_sum
+        """
+        connect = sqlite3.connect('data/data.db')
+        cur = connect.cursor()
+        cur.execute("SELECT count(Attendance_value) as 'sum_attendance' from Student_Attendance "
+                    "where student_id = (?)", student_id)
+        connect.commit()
+        attendance_by_student_id_sum = cur.fetchall()
+        return attendance_by_student_id_sum
