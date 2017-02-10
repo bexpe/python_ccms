ccbd5e4657e515f86a43975eb138845a0e623952
Beata <beata.k.puzio@gmail.com>
2017-02-10 09:53:21 +0100

    changes added to another branch

--- model/mentor_model.py
+++ model/mentor_model.py
@@ -8,7 +8,7 @@ class Mentor_model:
         self.c = self.conn.cursor()
 
     def get_list_of_mentors(self):
-        mentor_list = []
+        mentor_list = []git
         for item in self.c.execute("SELECT * FROM Mentor"):
             mentor_list.append(item)
         return mentor_list
