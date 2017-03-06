1a60b13140f351c941c1ba74e130249f1b3c286a
sebmichalowski <seb.michalowski@gmail.com>
2017-03-06 12:50:28 +0100

    assignment model

--- model/assignment_model.py
+++ model/assignment_model.py
@@ -5,8 +5,12 @@ class AssignmentAnswerModel:
     """
     Connects Assignment Class to database.
     """
-    @staticmethod
-    def set_assignment_answer_obj(student_solution_link, grade, student_id, assignment_id):
+    def __init__(self):
+        self.connect = sqlite3.connect("todo.db")
+        self.cur = self.connect.cursor()
+
+
+    def set_assignment_answer_obj(self,student_solution_link, grade, student_id, assignment_id):
         """
         Add answer object to database.
         :param student_solution_link:
@@ -16,45 +20,41 @@ class AssignmentAnswerModel:
         :return: None
         """
         try:
-            connect = sqlite3.connect('baza_danych.db')
-            cur = connect.cursor()
-            cur.execute("INSERT INTO Answers(Answer_text,Grade,Student_ID,Assignment_id) VALUES(?,?,?,?)",
-                        (student_solution_link, grade, student_id, assignment_id))
-            connect.commit()
-        except sqlite3.Error as err:
-            if connect:
-                connect.rollback()
-                print('There was a problem with SQL Data Base') .format(err)
-        finally:
-            if connect:
-                connect.close()
+            self.cur.execute("INSERT INTO Answers(Answer_text,Grade,Student_ID,Assignment_id) VALUES(?,?,?,?)",
+                            (student_solution_link, grade, student_id, assignment_id))
+            self.connect.commit()
 
-    @staticmethod
-    def get_assignment_answer_obj(student_id, assignment_id):
+        except sqlite3.OperationalError as err:
+            print("Cant add/edit this {}".format(err))
+
+        except sqlite3.Error as err:
+            if self.connect:
+                self.connect.rollback()
+                print('There was a problem with SQL Data Base {}') .format(err)
+
+
+    def get_assignment_answer_obj(self,student_id, assignment_id):
         """
         DB sample output [(2, 'Circle is round', 5, 1, 2)] : as in Constructor of AssignmentAnswer body
         :param student_id:
         :param assignment_id:
         :return:
         """
-        try:
-            connect = sqlite3.connect('baza_danych.db')
-            cur = connect.cursor()
-            cur.execute("SELECT * FROM Answers WHERE Student_ID=(?) AND Assignment_id=(?)",
-                        (student_id, assignment_id))
-            connect.commit()
-            answer = cur.fetchall()
-            return answer
-        except sqlite3.Error as err:
-            if connect:
-                connect.rollback()
-                print('There was a problem with SQL Data Base: {}') .format(err)
-        finally:
-            if connect:
-                connect.close()
 
-    @staticmethod
-    def update_assignment_answer_obj(answer_id, student_solution_link, grade, student_id, assignment_id):
+        try:
+            self.cur.execute("SELECT * FROM Answers WHERE Student_ID=(?) AND Assignment_id=(?)",
+                        (student_id, assignment_id))
+            self.connect.commit()
+
+        except sqlite3.OperationalError as err:
+            print("Cant add/edit this {}".format(err))
+
+        except sqlite3.Error as err:
+            if self.connect:
+                self.connect.rollback()
+                print('There was a problem with SQL Data Base {}') .format(err)
+
+    def update_assignment_answer_obj(self, answer_id, student_solution_link, grade, student_id, assignment_id):
         """
         Update answer object in database.
         :param answer_id:
@@ -64,18 +64,16 @@ class AssignmentAnswerModel:
         :param assignment_id:
         :return:
         """
+
         try:
-            connect = sqlite3.connect('baza_danych.db')
-            cur = connect.cursor()
-            cur.execute("UPDATE Answers SET Answer_text=(?), Grade=(?), Student_ID=(?), Assignment_id=(?) WHERE ID=(?)",
+            self.cur.execute("UPDATE Answers SET Answer_text=(?), Grade=(?), Student_ID=(?), Assignment_id=(?) WHERE ID=(?)",
                         (student_solution_link, grade, student_id, assignment_id, answer_id))
-            connect.commit()
-            answer = cur.fetchall()
-            return answer
+            self.connect.commit()
+
+        except sqlite3.OperationalError as err:
+            print("Cant add/edit this {}".format(err))
+
         except sqlite3.Error as err:
-            if connect:
-                connect.rollback()
-                print('There was a problem with SQL Data Base') .format(err)
-        finally:
-            if connect:
-                connect.close()
+            if self.connect:
+                self.connect.rollback()
+                print('There was a problem with SQL Data Base {}') .format(err)
