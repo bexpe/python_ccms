a7ac253000a74f7f62369c4c2b5591abfa0f1e6d
Stefan Kania <stefankania95@gmail.com>
2017-03-07 11:32:40 +0100

    database method created and one database.db file in model for tests copied

--- model/database.py
+++ model/database.py
@@ -2,4 +2,24 @@ import sqlite3
 
 
 class Database:
-    pass
+
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def set(self, command):
+        self.cursor.execute(command)
+        self.conn.commit()
+
+    def get(self, command):
+        self.cursor.execute(command)
+        data = self.cursor.fetchall()
+        return data
+
+# db = Database()
+# db.command("sdasdasd")
+# db.close_database()
+# db.get("select ")
