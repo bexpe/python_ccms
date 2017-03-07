63db54b15be749560d1eea25606125cfac760f9a
sebmichalowski <seb.michalowski@gmail.com>
2017-03-07 14:30:15 +0100

    mentor methods

--- model/mentor.py
+++ model/mentor.py
@@ -4,3 +4,18 @@ from model.user import User
 class Mentor(User):
     def __init__(self,user_id, name, surname, email, date_of_birth, city, phone, login):
         super().__init__(user_id, name, surname, email, date_of_birth, city, phone, login)
+
+    def get_mentor_by_id(self, id):
+        pass
+
+    def get_mentor_details(self, id):
+        pass
+
+    def get_list_of_mentors(self):
+        pass
+
+    def edit_mentor(self, id):
+        pass
+
+    def add_new_mentor(self):
+        pass
--- model/student.py
+++ model/student.py
@@ -1,8 +1,8 @@
 from model.user import User
 
 class Student(User):
-    def __init__(self, user_id, name, surname, email, date_of_birth, city, phone, login, password, team_id, card):
-        super().__init__(user_id, name, surname, email, date_of_birth, city, phone, login, password)
+    def __init__(self, user_id, name, surname, email, date_of_birth, city, phone, login, team_id, card):
+        super().__init__(user_id, name, surname, email, date_of_birth, city, phone, login)
         self.team_id = team_id
         self.card = card
 
