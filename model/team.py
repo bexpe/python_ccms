2dac542384029d17b7d5779f65664f24320479cb
Beata <beata.k.puzio@gmail.com>
2017-03-07 15:40:19 +0100

    changes in database.py

--- model/team.py
+++ model/team.py
@@ -3,13 +3,22 @@ from model.database import *
 
 class Team:
 
-    def __init__(self):
+    def __init__(self, team_id, team_name):
         self.db = Database()
-
-    def add_new_team(self, name):
-        self.db.set("INSERT INTO Teams VALUES (NULL, (?))", (name,))
+        self.team_id = team_id
+        self.team_name = team_name
+    @classmethod
+    def add_new_team(cls, name):
+        db = Database()
+        new_team = Team(0, name)
+        query = """INSERT INTO Teams(Name) VALUES (?)""", (new_team.name,)
+        db.set(query)
+        db.close()
+        return new_team
 
     def edit_team(self, team_id, edit_new_name):
+        db = Database()
+
         z = self.db.set("UPDATE Teams SET Name = (?) WHERE ID=(?)", (edit_new_name, team_id))
 
     def remove_team(self, team_id):
@@ -18,8 +27,16 @@ class Team:
     def get_team_details(self, team_id):
         self.db.get("SELECT * FROM Teams WHERE ID=(?)", (team_id,))
 
-    def get_list_of_teams(self):
-        self.db.get("SELECT * FROM Teams")
+    @classmethod
+    def get_list_of_teams(cls):
+        list_of_teams = []
+        db = Database()
+        query = """SELECT * FROM Teams"""
+        for team in db.get(query):
+            team_object = Team(team[0], team[1])
+            list_of_teams.append(team_object)
+        db.close()
+        return list_of_teams
 
     def add_student_to_team(self, student_id, team_id):
         self.db.get(" Teams ")
@@ -28,3 +45,4 @@ class Team:
 edit = Team()
 edit.edit_team(1, 'xd')
 edit.add_new_team('chvhv')
+print(edit.get_list_of_teams())
