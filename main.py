4991cf101135be2d00f70743d5b702d262f6999c
Tomek Bujakowski <bujakowski@gmail.com>
2017-03-08 10:59:11 +0100

    Add dump.db

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect
 from model.student import Student
 from model.mentor import Mentor
-
+import sys
+from dump_db import dump_db
 app = Flask(__name__)
+
+def check_run_args():
+    try:
+        if sys.argv[1] == '-d':
+            dump_db() # clearing db and inserting testing rows
+    except IndexError:
+        pass
 
 
 @app.route("/")
@@ -36,4 +44,5 @@ def mentor_list():
 
 
 if __name__ == "__main__":
+    check_run_args()
     app.run(debug=True, port=1111)
