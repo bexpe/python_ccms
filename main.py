b28935fbe8de34f743c165738bca719cc55ebbc4
Tomek Bujakowski <bujakowski@gmail.com>
2017-03-09 00:30:36 +0100

    Add actions to buttons in student_list

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import sys
 
 app = Flask(__name__)
 
+
 def check_run_args():
     try:
         if sys.argv[1] == '-d':
             from dump_db import dump_db
-            dump_db() # clearing db and inserting testing rows
+            dump_db()  # clearing db and inserting testing rows
     except IndexError:
         pass
 
@@ -24,14 +25,15 @@ def index():
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
-    #Todo.close_database()
+    # Todo.close_database()
     pass
+
 
 def redirect_url():
     """Return link to previous page"""
     return request.args.get('next') or \
-        request.referrer or \
-        url_for('index')
+           request.referrer or \
+           url_for('index')
 
 
 @app.route('/student_list.html')
@@ -44,6 +46,21 @@ def mentor_list():
     return render_template('mentor_list.html', mentors=Mentor.get_list_of_mentors())
 
 
+@app.route('/edit_student/<int:user_id>')
+def edit_student(user_id):
+    return render_template('edit_student.html', person=Student.get_student_by_id(user_id))
+
+
+@app.route('/details_student/<int:user_id>')
+def details_student(user_id):
+    return render_template('details_student.html', person=Student.get_student_by_id(user_id))
+
+
+@app.route('/add_student.html')
+def add_student():
+    return render_template('add_student.html')
+
+
 if __name__ == "__main__":
     check_run_args()
-    app.run(debug=True, port=1111)
+    app.run(debug=True, port=2222)
