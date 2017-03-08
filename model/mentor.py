fd8a37f56bd4f1a27cc71e0d11cc79bf8b0a03e1
Tomek Bujakowski <bujakowski@gmail.com>
2017-03-08 15:21:03 +0100

    Add save person to db

--- model/mentor.py
+++ model/mentor.py
@@ -3,18 +3,20 @@ from model.database import Database
 
 
 class Mentor(User):
-    def __init__(self,user_id, name, surname, email, date_of_birth, city, phone, login):
+    def __init__(self, user_id, name, surname, email, date_of_birth, city, phone, login):
         super().__init__(user_id, name, surname, email, date_of_birth, city, phone, login)
 
-    def get_mentor_by_id(self, idx):
+    @classmethod
+    def get_mentor_by_id(cls, idx):
         db = Database()
         query = """SELECT * FROM Mentor WHERE id =(?)"""
-        person = db.get(query, (idx,))
+        person = db.get(query, (idx,))[0]
+        print(person)
         new_mentor = Mentor(person[0], person[1], person[2], person[3], person[4], person[5], person[6], person[7])
         return new_mentor
 
     def get_mentor_details(self, idx):
-        pass
+        return self.__dict__
 
     @classmethod
     def get_list_of_mentors(cls):
@@ -22,7 +24,8 @@ class Mentor(User):
         db = Database()
         query = """SELECT * FROM Mentor;"""
         for person in db.get(query):
-            person_object = Mentor(person[0], person[1], person[2], person[3], person[4], person[5], person[6], person[7])
+            person_object = Mentor(person[0], person[1], person[2], person[3], person[4], person[5], person[6],
+                                   person[7])
             list_of_mentors.append(person_object)
 
         db.close()
@@ -32,28 +35,43 @@ class Mentor(User):
         db = Database()
         query = """UPDATE Mentor SET Name=(?), Surname=(?), Email=(?), Date_of_birth=(?), City=(?), Phone=(?), Login=(?) WHERE id =(?);"""
         db.get(query, (
-                        mentor_object.name,
-                        mentor_object.surname,
-                        mentor_object.email,
-                        mentor_object.date_of_birth,
-                        mentor_object.city,
-                        mentor_object.phone,
-                        mentor_object.login,
-                        mentor_object.user_id
-                        )
-                )
+            mentor_object.name,
+            mentor_object.surname,
+            mentor_object.email,
+            mentor_object.date_of_birth,
+            mentor_object.city,
+            mentor_object.phone,
+            mentor_object.login,
+            mentor_object.user_id
+        )
+               )
 
     def add_new_mentor(self, mentor_object):
         db = Database()
         query = """INSERT INTO Mentor(Name, Surname, Email, Date_of_birth, City, Phone, Login) VALUES (?,?,?,?,?,?,?);"""
         db.get(query, (
-                        mentor_object.name,
-                        mentor_object.surname,
-                        mentor_object.email,
-                        mentor_object.date_of_birth,
-                        mentor_object.city,
-                        mentor_object.phone,
-                        mentor_object.login,
-                        mentor_object.user_id
-                        )
-                )
+            mentor_object.name,
+            mentor_object.surname,
+            mentor_object.email,
+            mentor_object.date_of_birth,
+            mentor_object.city,
+            mentor_object.phone,
+            mentor_object.login,
+            mentor_object.user_id
+        )
+               )
+    def save(self):
+        db = Database()
+        values = (
+        self.name, self.surname, self.email, self.date_of_birth, self.city, self.phone, self.login, self.user_id)
+
+        if not self.user_id:
+            values = values[:-1]
+            query = """INSERT INTO Mentor(Name, Surname, Email, Date_of_birth, City, Phone, Login) VALUES (?,?,?,?,?,?,?);"""
+        else:
+            query = """UPDATE Mentor SET Name=(?), Surname=(?), Email=(?), Date_of_birth=(?), City=(?), Phone=(?), Login=(?) WHERE id =(?);"""
+
+        db.set(query, values)
+        db.close()
+
+
