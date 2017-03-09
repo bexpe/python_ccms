2b1c03db87cc8cf3e484e64e6f38420edfab5353
Beata <beata.k.puzio@gmail.com>
2017-03-09 10:20:03 +0100

    init to user added

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request, redirect
-from model.student import Student
+# from model.student import Student
 from model.mentor import Mentor
 from model.team import Team
+import sys
 
 app = Flask(__name__)
+
+
+def check_run_args():
+    try:
+        if sys.argv[1] == '-d':
+            from dump_db import dump_db
+            dump_db() # clearing db and inserting testing rows
+    except IndexError:
+        pass
 
 
 @app.route("/")
@@ -12,11 +22,6 @@ def index():
     """
     return render_template('index.html')
 
-@app.route('/teams')
-def (error):
-    """Closes the database again at the end of the request."""
-    #Todo.close_database()
-    pass
 
 @app.teardown_appcontext
 def close_db(error):
@@ -30,9 +35,22 @@ def redirect_url():
         request.referrer or \
         url_for('index')
 
+
+@app.route('/student_list.html')
+def student_list():
+    return render_template('student_list.html', students=Student.get_list_of_students())
+
+
+@app.route('/mentor_list.html')
+def mentor_list():
+    return render_template('mentor_list.html', mentors=Mentor.get_list_of_mentors())
+
+
 @app.route('/teams.html')
 def teams():
     return render_template('teams.html', teams=Team.get_list_of_teams())
 
+
 if __name__ == "__main__":
-    app.run()
+    check_run_args()
+    app.run(debug=True, port=1111)
--- model/mentor.py
+++ model/mentor.py
@@ -1,4 +1,4 @@
-import model.user
+from model.user import *
 
 
 class Mentor(User):
