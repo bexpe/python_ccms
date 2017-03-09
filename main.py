956bd19a145b19a4eefd15a5b53bb836fd212563
Stefan Kania <stefankania95@gmail.com>
2017-03-09 10:55:12 +0100

    First session files, need commit to merge with students

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, session, url_for, escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    """ Shows list of todo items stored in the database.
-    """
-    return render_template('index.html')
+    if 'username' in session:
+        session['username']
+        return render_template('index.html', user=user)
+    return 'You are not logged in'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template("login.html")
 
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    #Todo.close_database()
-    pass
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
-def redirect_url():
-    """Return link to previous page"""
-    return request.args.get('next') or \
-        request.referrer or \
-        url_for('index')
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
 if __name__ == "__main__":
