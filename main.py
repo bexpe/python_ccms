15b09554549379cb85ee3d6b330e12b773ce9393
komuher <komuher@gmail.com>
2017-03-09 19:28:51 +0100

    jinja changes and new layout menu

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/attendance_data/<student_id>?<start_date>?<end_date>')
 def attendance_data(student_id, start_date, end_date):
+    user = session['user']
     attendance = Attendance.check_attendance_by_date(start_date, end_date, student_id)
     attendance_lvl = 0
     x = 0
@@ -26,13 +27,14 @@ def attendance_data(student_id, start_date, end_date):
     if x != 0:
         attendance_lvl = attendance_lvl / x
         attendance_lvl = int(attendance_lvl)
-    return render_template("attendance.html", rows=attendance, attendence_lvl=attendance_lvl, student_id=student_id)
+    return render_template("attendance.html", user=user, rows=attendance, attendence_lvl=attendance_lvl, student_id=student_id)
 
 
 @app.route('/attendance/<student_id>')
 def attendance(student_id):
     attendance = Attendance.get_student_attendance(student_id)
     attendance_lvl = 0
+    user = session['user']
     x = 0
     for i in attendance:
         if i[1] == "Obecny":
@@ -48,17 +50,19 @@ def attendance(student_id):
     if x != 0:
         attendance_lvl = attendance_lvl / x
         attendance_lvl = int(attendance_lvl)
-    return render_template("attendance.html", rows=attendance, attendence_lvl=attendance_lvl, student_id=student_id)
+    return render_template("attendance.html", user=user, rows=attendance, attendence_lvl=attendance_lvl, student_id=student_id)
 
 
 @app.route('/student_list.html')
 def student_list():
-    return render_template('student_list.html', students=Student.get_list_of_students())
+    user = session['user']
+    return render_template('student_list.html',user=user, students=Student.get_list_of_students())
 
 
 @app.route('/check_attendance', methods=["POST", 'GET'])
 def check_attendance():
-    return render_template('check_attendance.html', students=Student.get_list_of_students())
+    user = session['user']
+    return render_template('check_attendance.html', students=Student.get_list_of_students(), user=user)
 
 
 @app.route('/present/<student_id>')
@@ -78,14 +82,21 @@ def late(student_id):
     Attendance.set_attendance(student_id, "Spozniony")
     return redirect(url_for('check_attendance'))
 
+@app.route('/check_everyone_attendance')
+def check_everyone_attendance():
+    user = session['user']
+    return render_template('check_everyone_attendance.html', user=user, rows=Attendance.check_everyone_attendance())
+
 @app.route('/attendance_by_data', methods=['GET', 'POST'])
 def data():
+    user = session['user']
     if request.method == "POST":
         start_date = request.form.get("start")
         end_date = request.form.get("end")
         student_id = request.form.get("student_id")
-        return redirect(url_for('attendance_data', student_id=student_id, start_date=start_date, end_date=end_date))
-    return render_template('attendance_by_data.html')
+        return redirect(url_for('attendance_data', user=user, student_id=student_id, start_date=start_date, end_date=end_date))
+    return render_template('attendance_by_data.html', user=user)
+
 
 def check_run_args():
     try:
