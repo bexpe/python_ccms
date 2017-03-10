1eeeba64c0d7a007c40d87317d61915c52a2e8b1
sebmichalowski <seb.michalowski@gmail.com>
2017-03-10 02:26:11 +0100

    sesion validation - NOT CHECKED

--- main.py
+++ main.py
@@ -36,107 +36,141 @@ def redirect_url():
 
 @app.route('/student_list.html')
 def student_list():
-    return render_template('student_list.html', students=Student.get_list_of_students())
+    user = session['user']
+    return render_template('student_list.html', students=Student.get_list_of_students(), user=user)
 
 @app.route('/mentor_list.html')
 def mentor_list():
-    return render_template('mentor_list.html', mentors=Mentor.get_list_of_mentors())
+    user = session['user']
+    return render_template('mentor_list.html', mentors=Mentor.get_list_of_mentors(), user=user)
 
 @app.route('/edit_mentor/<int:user_id>', methods=['GET', 'POST'])
 def edit_mentor(user_id):
-    mentor_url = "mentor_list"
-    if request.method == "POST":
-        login = request.form['login']
-        email = request.form['email']
-        name = request.form['name']
-        surname = request.form['surname']
-        date_of_birth = None
-        city = None
-        phone = None
-        new_mentor = Mentor(user_id, name, surname, email, date_of_birth, city, phone, login)
-        new_mentor.save()
-        return redirect(url_for(mentor_url))
-    return render_template('edit_mentor.html', person=Mentor.get_mentor_by_id(user_id), url=mentor_url)
+    user = session['user']
+    if user['type'] == 'Manager':
+        mentor_url = "mentor_list"
+        if request.method == "POST":
+            login = request.form['login']
+            email = request.form['email']
+            name = request.form['name']
+            surname = request.form['surname']
+            date_of_birth = None
+            city = None
+            phone = None
+            new_mentor = Mentor(user_id, name, surname, email, date_of_birth, city, phone, login)
+            new_mentor.save()
+            return redirect(url_for(mentor_url))
+        return render_template('edit_mentor.html', person=Mentor.get_mentor_by_id(user_id), url=mentor_url)
+    return redirect(url_for('error.html'))
 
 @app.route('/edit_student/<int:user_id>', methods=['GET', 'POST'])
 def edit_student(user_id):
-    student_url = "student_list"
-    if request.method == "POST":
-        login = request.form['login']
-        email = request.form['email']
-        name = request.form['name']
-        surname = request.form['surname']
-        date_of_birth = None
-        city = None
-        phone = None
-        team_id = None
-        card = None
-        new_student = Student(user_id, name, surname, email, date_of_birth, city, phone, login, team_id, card)
-        new_student.save()
-        return redirect(url_for(student_url))
-    return render_template('edit_student.html', person=Student.get_student_by_id(user_id), url=student_url)
+    user = session['user']
+    if user['type'] == 'Manager' or 'Mentor' or "Employee":
+        student_url = "student_list"
+        if request.method == "POST":
+            login = request.form['login']
+            email = request.form['email']
+            name = request.form['name']
+            surname = request.form['surname']
+            date_of_birth = None
+            city = None
+            phone = None
+            team_id = None
+            card = None
+            new_student = Student(user_id, name, surname, email, date_of_birth, city, phone, login, team_id, card)
+            new_student.save()
+            return redirect(url_for(student_url))
+        return render_template('edit_student.html', person=Student.get_student_by_id(user_id), url=student_url)
+    return redirect(url_for('error.html'))
 
 @app.route('/add_mentor.html', methods=['GET', 'POST'])
 def add_mentor():
-    mentor_url = "mentor_list"
-    if request.method == "POST":
-        user_id = None
-        login = request.form['login']
-        email = request.form['email']
-        name = request.form['name']
-        surname = request.form['surname']
-        date_of_birth = None
-        city = None
-        phone = None
-        new_mentor = Mentor(user_id, name, surname, email, date_of_birth, city, phone, login)
-        new_mentor.save()
-        return redirect(url_for(mentor_url))
-    return render_template('add_mentor.html', url=mentor_url)
+    user = session['user']
+    if user['type'] == 'Manager':
+        mentor_url = "mentor_list"
+        if request.method == "POST":
+            user_id = None
+            login = request.form['login']
+            email = request.form['email']
+            name = request.form['name']
+            surname = request.form['surname']
+            date_of_birth = None
+            city = None
+            phone = None
+            new_mentor = Mentor(user_id, name, surname, email, date_of_birth, city, phone, login)
+            new_mentor.save()
+            return redirect(url_for(mentor_url))
+        return render_template('add_mentor.html', url=mentor_url)
+    return redirect(url_for('error.html'))
 
 @app.route('/add_student.html', methods=['GET', 'POST'])
 def add_student():
-    student_url = "student_list"
-    if request.method == "POST":
-        user_id = None
-        login = request.form['login']
-        email = request.form['email']
-        name = request.form['name']
-        surname = request.form['surname']
-        date_of_birth = None
-        city = None
-        phone = None
-        team_id = None
-        card = None
-        new_student = Student(user_id, name, surname, email, date_of_birth, city, phone, login, team_id, card)
-        new_student.save()
-        return redirect(url_for(student_url))
-    return render_template('add_student.html', url=student_url)
+    user = session['user']
+    if user['type'] == 'Manager' or 'Mentor' or "Employee":
+        student_url = "student_list"
+        if request.method == "POST":
+            user_id = None
+            login = request.form['login']
+            email = request.form['email']
+            name = request.form['name']
+            surname = request.form['surname']
+            date_of_birth = None
+            city = None
+            phone = None
+            team_id = None
+            card = None
+            new_student = Student(user_id, name, surname, email, date_of_birth, city, phone, login, team_id, card)
+            new_student.save()
+            return redirect(url_for(student_url))
+        return render_template('add_student.html', url=student_url)
+    return redirect(url_for('error.html'))
 
 @app.route('/details_mentor/<int:user_id>')
 def details_mentor(user_id):
-    mentor_url = "mentor_list"
-    return render_template('details_mentor.html', person=Mentor.get_mentor_by_id(user_id), url=mentor_url)
+    user = session['user']
+    if user['type'] == 'Manager':
+        mentor_url = "mentor_list"
+        return render_template('details_mentor.html', person=Mentor.get_mentor_by_id(user_id), url=mentor_url)
+    return redirect(url_for('error.html'))
 
 @app.route('/details_student/<int:user_id>')
 def details_student(user_id):
-    student_url = "student_list"
-    return render_template('details_student.html', person=Student.get_student_by_id(user_id), url=student_url)
+    user = session['user']
+    if user['type'] == 'Manager' or 'Manager' or "Employee":
+        student_url = "student_list"
+        return render_template('details_student.html', person=Student.get_student_by_id(user_id), url=student_url)
+    return redirect(url_for('error.html'))
 
 @app.route('/remove_mentor/<int:user_id>')
 def remove_mentor(user_id):
-    mentor = Mentor.get_mentor_by_id(user_id)
-    mentor.delete()
-    return redirect(url_for('mentor_list'))
+    user = session['user']
+    if user['type'] == 'Manager':
+        mentor = Mentor.get_mentor_by_id(user_id)
+        mentor.delete()
+        return redirect(url_for('mentor_list'))
+    return redirect(url_for('error.html'))
 
 @app.route('/remove_student/<int:user_id>')
 def remove_student(user_id):
-    student = Student.get_student_by_id(user_id)
-    student.delete()
-    return redirect(url_for('student_list'))
+    user = session['user']
+    if user['type'] == 'Manager' or 'Mentor' or "Employee":
+        student = Student.get_student_by_id(user_id)
+        student.delete()
+        return redirect(url_for('student_list'))
+    return redirect(url_for('error.html'))
 
 @app.route('/add_to_team/<int:user_id>')
 def add_to_team(user_id):
-    pass
+    user = session['user']
+    if user['type'] == 'Manager' or 'Mentor' or "Employee":
+        pass # TODO BEATA teams
+    return redirect(url_for('error.html'))
+
+@app.route('/error.html.html')
+def privileges_error_handler():
+    return render_template('error.html')
+
 
 if __name__ == "__main__":
     check_run_args()
