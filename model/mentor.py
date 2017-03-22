fcbc91bb7d0d6bd5d98052e1fb6c68c822c831fa
Tomek Bujakowski <bujakowski@gmail.com>
2017-03-22 12:57:45 +0100

    Fix init arguments in model/mentor.py

--- model/mentor.py
+++ model/mentor.py
@@ -19,7 +19,7 @@ class Mentor(User, db.Model):
     password = db.Column(db.String)
 
     def __init__(self, user_id, name, surname, email, date_of_birth, city, phone, login):
-        User.__init__(user_id, name, surname, email, date_of_birth, city, phone, login)
+        super().__init__(user_id, name, surname, email, date_of_birth, city, phone, login)
 
     @classmethod
     def get_mentor_by_id(cls, idx):
