36d7c7d88b6a01820f5dbbdd724b0fe8c8ceccec
Tomek Bujakowski <bujakowski@gmail.com>
2017-03-24 00:39:05 +0100

    Define columns in manager model

--- model/manager.py
+++ model/manager.py
@@ -1,5 +1,21 @@
-import model.user
+from model.user import User
+from main import db
 
+class Manager(User, db.Model):
 
-class Manager(User):
-    pass
+    # table name in database for SQLAlchemy
+    __tablename__ = 'Manager'
+
+    # columns in table for SQLAlchemy
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    email = db.Column(db.String)
+    date_of_birth = db.Column(db.String)
+    city = db.Column(db.String)
+    phone = db.Column(db.String)
+    login = db.Column(db.String)
+    password = db.Column(db.String)
+
+    def __init__(self, user_id, name, surname, email, date_of_birth, city, phone, login):
+        super().__init__(user_id, name, surname, email, date_of_birth, city, phone, login)
